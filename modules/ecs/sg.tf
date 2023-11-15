d792ef4dee31e60f4f309e23d466b7f05db581a9
HyunSu1768 <azxcv1768@gmail.com>
2023-11-15 19:54:51 +0900

    feat :: Add Ingress Rule 443, 80

--- modules/ecs/sg.tf
+++ modules/ecs/sg.tf
@@ -1,6 +1,20 @@
 resource "aws_security_group" "lb" {
   vpc_id = var.vpc_id
   name   = "lb-sg"
+  ingress {
+    from_port = 443
+    protocol  = "tcp"
+    to_port   = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol  = "-1"
